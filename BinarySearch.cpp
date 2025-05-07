0da85483973172f42df8d64eec0254ce4d3f2132
Indra2512 <mononeon1225@gmail.com>
2025-05-07 15:52:55 +0700

    memasukkan banyakanya array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,3 +5,28 @@ int element[10];
 int nPanjang;
 int x;
 
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyakanya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] jumlah elemen tidak boleh lebih dari 10. silahkan coba lagi.\n";
+        }
+    }
+
+    cout << "\n====================================\n";
+    cout << "           Masukkan Elemen Array    \n";
+    cout << "====================================\n";
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << "=";
+        cin >> element[i];
+    }
+}
